14360fcd4d7bc0164d198fdf80048332abefb0d5
Lee <houlee0211@gmail.com>
2022-06-03 11:36:40 +0800

    Revert "second upload"
    This reverts commit b6fdf218f9a8d86e0389a6c8c2344ab48f82800f.

--- GitUpload/src/GitTest/MathTest.java
+++ GitUpload/src/GitTest/MathTest.java
@@ -4,11 +4,7 @@ public class MathTest {
 
 	public static void main(String[] args) {
 		System.out.println("上傳第一次!!!");
-		Math.add(10, 30);
+
 	}
-}
-class Math{
-	static int add(int x, int y) {
-		return x + y;
-	}
+
 }
