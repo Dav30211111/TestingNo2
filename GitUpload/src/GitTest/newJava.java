edd4f47a17f950a3d3f8ab427a7affb2138e6e75
Lee <houlee0211@gmail.com>
2022-06-03 11:57:05 +0800

    12312313213

--- GitUpload/src/GitTest/newJava.java
+++ GitUpload/src/GitTest/newJava.java
@@ -4,7 +4,5 @@ public class newJava {
 
 	public static void main(String[] args) {
 		System.out.println("我是組員，新增資料囉～～");
-
 	}
-
 }
---
+++ GitUpload/src/GitTest/wwwwwww.java
@@ -0,0 +1,10 @@
+package GitTest;
+
+public class wwwwwww {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
