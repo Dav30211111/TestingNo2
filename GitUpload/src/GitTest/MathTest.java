b6fdf218f9a8d86e0389a6c8c2344ab48f82800f
Lee <houlee0211@gmail.com>
2022-06-03 11:25:24 +0800

    second upload

--- GitUpload/src/GitTest/MathTest.java
+++ GitUpload/src/GitTest/MathTest.java
@@ -4,7 +4,11 @@ public class MathTest {
 
 	public static void main(String[] args) {
 		System.out.println("上傳第一次!!!");
-
+		Math.add(10, 30);
 	}
-
+}
+class Math{
+	static int add(int x, int y) {
+		return x + y;
+	}
 }
